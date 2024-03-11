1a674f735a011c52710fc93281d6fedf82e0e7dd
Adhayan Sharma <adhayan436@gmail.com>
2024-03-11 21:39:54 +0530

    'first'

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { View, Text } from 'react-native'
+import React, { useState,useMemo} from 'react'
+import { Link,Stack } from 'expo-router'
+import ExploreHeader from '@/components/ExploreHeader'
+import Listings from '@/components/Listings'
+import listingsdata from '@/assets/data/airbnb-listings.json'
+import listingsdatageo from '@/assets/data/airbnb-listings.geo.json'
+import ListingsMap from '@/components/ListingsMap'
+import ListingsBottomSheet from '@/components/ListingsBottomSheet'
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+const index = () => {
+  const [category,setCategory]=useState('Tiny Homes')
+  const items=useMemo(()=>listingsdata as any,[])
+  const onDataChanged=(category:string)=>{
+    console.log("CHANGED:",category)
+    setCategory(category)
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={{flex:1}}>
+       <Stack.Screen options={{header:()=>(<ExploreHeader onCategoryChanged={onDataChanged}/>)}}/>
+       {/*<Listings listings={items} category={category}/>*/}
+       <ListingsMap listings={listingsdatageo}/>
+       <ListingsBottomSheet listings={items} category={category}/>
     </View>
-  );
+    
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default index
